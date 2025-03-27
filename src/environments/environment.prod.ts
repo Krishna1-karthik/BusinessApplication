2d24375a6aea7dc1b385047bbc1d495f3056ce30
karthik130487 <159273619+karthik130487@users.noreply.github.com>
2025-03-27 12:13:46 +0530

    lat code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    API_KEY: 'AlzaSy-jduE11xr_YpIm8w412sVvf3oyl5hHSzb',
-    baseUrl:'https://business-11.onrender.com/api/'
+    API_KEY: 'AlzaSyvu4wBq65fYod_J0A3fgvfqGNJwbB-NiRY',
+    baseUrl:'https://businessnewenvironment.onrender.com/api/'
 
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    API_KEY: 'AlzaSyir21Da0yH0Hqim58n4-kQgFl_Zcp7zJ6z',
-    baseUrl:'https://localhost:7000/api/'
+    API_KEY: 'AlzaSy3dIE5AOEcgLZPCQR8AGO69_xOKON39aQs',
+    baseUrl:'https://localhost:44368/api/'
   };
