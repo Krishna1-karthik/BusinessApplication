622a82000e4afd2391da412aac5dff81d0b9602b
karthik130487 <159273619+karthik130487@users.noreply.github.com>
2025-06-17 21:54:37 +0530

    url changes for environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,4 @@ export const environment = {
     production: true,
     API_KEY: 'AlzaSy-X2lm0XOuzNdG-d9e8aLMdyzKZlt077EH',
     baseUrl:'https://businessnewenvironment.onrender.com/api/'
-
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     API_KEY: 'AlzaSydVXrV5WtrktpDT75LxYj0WiBrqpkcgJTe',
-    baseUrl:'https://localhost:44368/api/'
+    // baseUrl:'https://localhost:44368/api/'
+    baseUrl:'https://businessnewenvironment.onrender.com/api/'
   };
